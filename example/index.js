4f2b2b9ee80c551eda22e19f25d70a1edf165866
youngheart12 <manishkumar199222@gmail.com>
2022-08-16 19:42:35 +0530

    initial commit

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-const RedcarpetUpAPI = require('../lib').RedcarpetUpAPI;
+const RedcarpetUpAPI = require('../lib/index');
 
 const redcarpetUpApi = new RedcarpetUpAPI({
     productType: "your-product-type",
@@ -6,134 +6,135 @@ const redcarpetUpApi = new RedcarpetUpAPI({
     appVersion: "app-version",
 });
 
-const getUserProfile = (params) => {
-    redcarpetUpApi.getUserProfile(params)
-        .then(response => {
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+redcarpetUpApi.isOk();
+// const getUserProfile = (params) => {
+//     redcarpetUpApi.getUserProfile(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const getUserCardDetails = (params) => {
-    redcarpetUpApi.getUserCardDetails(params)
-        .then(response => {
-            // returns the list of cards user has disbursed
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+// const getUserCardDetails = (params) => {
+//     redcarpetUpApi.getUserCardDetails(params)
+//         .then(response => {
+//             // returns the list of cards user has disbursed
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const getUserStatements = (params) => {
-    redcarpetUpApi.getUserStatements(params)
-        .then(response => {
-            // returns the list of statements -: Billed, Unbilled, Payments, Emis, etc.
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+// const getUserStatements = (params) => {
+//     redcarpetUpApi.getUserStatements(params)
+//         .then(response => {
+//             // returns the list of statements -: Billed, Unbilled, Payments, Emis, etc.
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const createPaymentRequest = (params) => {
-    redcarpetUpApi.createPaymentRequest(params)
-        .then(response => {
-            // returns payment data information key, id etc.
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+// const createPaymentRequest = (params) => {
+//     redcarpetUpApi.createPaymentRequest(params)
+//         .then(response => {
+//             // returns payment data information key, id etc.
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const fulfillPaymentRequest = (params) => {
-    redcarpetUpApi.capturePayment(params)
-        .then(response => {
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+// const fulfillPaymentRequest = (params) => {
+//     redcarpetUpApi.capturePayment(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const setUserPersonalInfo = (params) => {
-    redcarpetUpApi.setUserPersonalInfo(params)
-        .then(response => {
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+// const setUserPersonalInfo = (params) => {
+//     redcarpetUpApi.setUserPersonalInfo(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// };
 
-const signUpUser = (params) => {
-    redcarpetUpApi.getOtp(params)
-        .then(response => {
-            if (response.result === "success") {
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
+// const signUpUser = (params) => {
+//     redcarpetUpApi.getOtp(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
 
-const verifyOtp = (params) => {
-    redcarpetUpApi.verifyOtp(params)
-        .then(response => {
-            if (response.result === "success") {
-                // users accessToken = response.access_token
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
+// const verifyOtp = (params) => {
+//     redcarpetUpApi.verifyOtp(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 // users accessToken = response.access_token
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
 
-const getUserFunnelStatus = (params) => {
-    redcarpetUpApi.getUserStatus(params)
-        .then(response => {
-            if (response.result === "success") {
-                // userProductId = response.data[0].user_product_id
-                // funnel = response.data[0].current_task
-                console.log(response);
-            } else {
-                console.log(response.message);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
+// const getUserFunnelStatus = (params) => {
+//     redcarpetUpApi.getUserStatus(params)
+//         .then(response => {
+//             if (response.result === "success") {
+//                 // userProductId = response.data[0].user_product_id
+//                 // funnel = response.data[0].current_task
+//                 console.log(response);
+//             } else {
+//                 console.log(response.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
